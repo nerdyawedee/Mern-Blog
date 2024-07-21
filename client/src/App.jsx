321e99cebae0d8235e075032b2c1af1ac29d3d03
patnaikdebasmita44@gmail.com <patnaikdebasmita44@gmail.com>
2024-07-21 20:30:39 +0530

    created and run server

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,26 @@
-import {BrowserRouter as Router,Routes,Route} from "react-router-dom"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Home from "./Component/Home"
+import 'flowbite/dist/flowbite.css';
 import About from "./Component/About"
 import Signin from "./Component/Signin"
 import Signup from "./Component/Signup"
 import Dashboard from "./Component/Dashboard"
 import Projects from "./Component/Projects"
- 
+import Header from "./Pages/Header.jsx"
+// import  "../node_modules/boo"
+// import './App.css'
+
 function App() {
   return (
     <Router>
+      <Header />
       <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="/about" element={<About/>} />
-        <Route path="/sign-in" element={<Signin/>} />
-        <Route path="/sign-up" element={<Signup/>} />
-        <Route path="/dashboard" element={<Dashboard/>} />
-        <Route path="/projects" element={<Projects/>} />
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<Signin />} />
+        <Route path="/sign-up" element={<Signup />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/projects" element={<Projects />} />
       </Routes>
     </Router>
   )
